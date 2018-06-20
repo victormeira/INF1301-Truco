8ba55a56ff051e9d5fc56b0e57aba0f34d582c7b
Alexandre Wanick <alexandre@wvieira.com>
2018-06-20 15:02:53 -0300

    Add files via upload

--- INF1301-Trab04-AWBRVM/Srcs/MESA/MESA.C
+++ INF1301-Trab04-AWBRVM/Srcs/MESA/MESA.C
@@ -52,10 +52,6 @@ typedef struct tgMesa {
 
 static int nJogadores = MAX_JOGADORES ;
 
-/***** Protótipos das funções encapuladas no módulo *****/
-
-static MES_tpCondRet MES_DefinirManilha ( MES_tppMesa pMesa, BAR_tpCarta * cartaManilha ) ;
-
 /***********************************************************************
  *  Função: MES Criar Mesa
  ***********************************************************************/
@@ -72,15 +68,12 @@ MES_tppMesa MES_CriarMesa ( int numJogadores )
 
     if ( numJogadores != 2 && numJogadores != 4 && numJogadores != 6 )
     {
-        //TODO: Tratar se o Num de Jogadores nao for 2,4,6
         return NULL ;
-    }
+    } /* if */
 
     nJogadores = numJogadores ;
     pMesa->proxAJogar = 0 ;
-
-    // defining a Manilha so it isn't NULL
-    pMesa->cartaManilha = BAR_CriarCarta( A, OUROS ) ;
+    pMesa->cartaManilha = NULL ;
     
     return pMesa ;
     /* Fim função: MES Criar Mesa */
@@ -92,28 +85,47 @@ MES_tppMesa MES_CriarMesa ( int numJogadores )
 
 MES_tpCondRet MES_DefinirManilha ( MES_tppMesa pMesa, BAR_tpCarta * cartaManilha )
 {
-    int *mValor, *mNaipe;
+    int mValor ;
+    int mNaipe ;
 
     if ( cartaManilha != NULL )
     {
-        //TODO: Tratar CondRet do ObterInfo
-        BAR_ObterInfo( cartaManilha , mValor , mNaipe ) ;
-
-        if ( mValor != NULL && mNaipe != NULL )
+        if ( BAR_ObterInfo( cartaManilha , &mValor , &mNaipe ) == BAR_CondRetCartaNaoExiste )
         {
-            pMesa->cartaManilha = BAR_CriarCarta( mValor , mNaipe ) ;
-            BAR_DestruirCarta( cartaManilha ) ;
+            return MES_CondRetCartaNaoExiste ;
+        } /* if */
+
+        if ( mValor >= A && mValor <= 6 )
+        {
+            mValor++ ;
+        }
+        else if ( mValor == 7 )
+        {
+            mValor = Q ;
+        }
+        else if ( mValor == Q )
+        {
+            mValor = J ;
+        }
+        else if ( mValor == J )
+        {
+            mValor = K ;
+        }
+        else if ( mValor == K )
+        {
+            mValor = A ;
         }
         else
         {
-            //TODO: Tratar se deu M no ObterInfo
             return MES_CondRetCartaNaoExiste ;
-        }
+        } /* if */
+
+        pMesa->cartaManilha = BAR_CriarCarta( mValor , mNaipe ) ;
     }
     else
     {
         return MES_CondRetCartaNaoExiste ;
-    }
+    } /* if */
 
     return MES_CondRetOk ;
     /* Fim função: MES Definir Manilha */
@@ -125,8 +137,6 @@ MES_tpCondRet MES_DefinirManilha ( MES_tppMesa pMesa, BAR_tpCarta * cartaManilha
 
 void MES_DestruirMesa ( MES_tppMesa pMesa )
 {
-    BAR_DestruirCarta( pMesa->pCartaManilha ) ;
-
     free( pMesa ) ;
     /* Fim função: MES Destruir Mesa */
 }
@@ -137,15 +147,31 @@ void MES_DestruirMesa ( MES_tppMesa pMesa )
 
 MES_tpCondRet MES_EsvaziarMesa ( MES_tppMesa pMesa )
 {
-    //TODO: Implement Esvaziar Mesa - Precisa mesmo??
     int i ;
+
+    static int primeiroDaRodada = 0 ;
+
+    if ( pMesa == NULL )
+    {
+        return MES_CondRetMesaNaoExiste ;
+    } /* if */
+
+    BAR_DestruirCarta( pMesa->pCartaManilha ) ;
 
     for ( i = 0 ; i < nJogadores ; i++ )
     {
-        BAR_DestruirCarta( pMesa->vtCartasNaMesa[ i ] ) ;
+        if ( BAR_DestruirCarta( pMesa->vtCartasNaMesa[ i ] ) == BAR_CondRetCartaNaoExiste )
+        {
+            return MES_CondRetCartaNaoExiste ;
+        } /* if */
 
         pMesa->vtCartasNaMesa[ i ] = NULL ;
     }
+
+    primeiroDaRodada = ( primeiroDaRodada + 1 ) % nJogadores ;
+    pMesa->proxAJogar = primeiroDaRodada ;
+
+    return MES_CondRetOk ;
     /* Fim função: MES Esvaziar Mesa */
 }
 
@@ -155,35 +181,30 @@ MES_tpCondRet MES_EsvaziarMesa ( MES_tppMesa pMesa )
 
 MES_tpCondRet MES_JogarCarta ( MES_tppMesa pMesa, MES_tppCarta pCarta, int jogadorId )
 {
-    int * mValor;
-    int * mNaipe;
+    int mValor ;
+    int mNaipe ;
 
     if ( pCarta == NULL )
     {
         return MES_CondRetCartaNaoExiste ;
-    }
+    } /* if */
 
     if ( pMesa == NULL )
     {
         return MES_CondRetMesaNaoExiste ;
-    }
+    } /* if */
 
     if ( jogadorId - 1 != pMesa->proxAJogar )
     {
-        //TODO: Trata o erro do prox a jogar nao é o jogador passado
         return MES_CondRetJogadorNaoExiste ;
-    }
+    } /* if */
 
-    //TODO: Tratar CondRet do ObterInfo
-    BAR_ObterInfo( pCarta , mValor , mNaipe ) ;
-
-    if ( mValor == NULL || mNaipe == NULL )
+    if ( BAR_ObterInfo( pCarta , &mValor , &mNaipe ) == BAR_CondRetCartaNaoExiste )
     {
-        //TODO: caso ObterInfo deu M
         return MES_CondRetCartaNaoExiste ;
-    }
+    } /* if */
 
-    pMesa->vtCartasNaMesa[ pMesa->proxAJogar ] = BAR_CriarCarta( * mValor , * mNaipe ) ;
+    pMesa->vtCartasNaMesa[ pMesa->proxAJogar ] = BAR_CriarCarta( mValor , mNaipe ) ;
 
     pMesa->proxAJogar = ( pMesa->proxAJogar + 1 ) % nJogadores ;
 
@@ -195,23 +216,72 @@ MES_tpCondRet MES_JogarCarta ( MES_tppMesa pMesa, MES_tppCarta pCarta, int jogad
  *  Função: MES Obter Carta
  ***********************************************************************/
 
-MES_tpCondRet MES_ObterCarta ( int jogadorId , int *pValor ,
-                                int *pNaipe ) ;
+MES_tpCondRet MES_ObterCarta ( int jogadorId , int * pValor , int * pNaipe )
 {
-    if(jogadorId < 1 || jogadorId > nJogadores){
+    if ( jogadorId < 1 || jogadorId > nJogadores )
+    {
         return MES_CondRetJogadorNaoExiste;
-    }
+    } /* if */
 
-    BAR_ObterInfo(pMesa->vtCartasNaMesa[jogadorId-1], pValor, pNaipe);
+    if ( BAR_ObterInfo( pMesa->vtCartasNaMesa[ jogadorId - 1 ] , pValor , pNaipe ) == BAR_CondRetCartaNaoExiste )
+    {
+        return MES_CondRetCartaNaoExiste ;
+    } /* if */
 
-    return MES_CondRetOk;
+    return MES_CondRetOk ;
+    /* Fim função: MES Obter Carta */
 }
 
 /***********************************************************************
  *  Função: MES Identificar Ganhador
  ***********************************************************************/
 
-MES_tpCondRet MES_IdentificarGanhador ( MES_tppMesa pMesa )
+MES_tpCondRet MES_IdentificarGanhador ( MES_tppMesa pMesa , int * timeGanhador )
 {
-    
+    int maiorCartaTime1 ;
+    int maiorCartaTime2 ;
+    int maiorCarta ;
+    int i ;
+
+    if ( pMesa == NULL )
+    {
+        return MES_CondRetMesaNaoExiste ;
+    } /* if */
+
+    maiorCartaTime1 = 0 ;
+    maiorCartaTime2 = 1 ; 
+
+    //compara 0 com 2, depois melhor com 4 e o mesmo pro segundo time
+    for ( i = 1 ;  i < nJogadores / 2 ; i++ )
+    {
+        if ( BAR_IdentificaMaior( pMesa->vtCartasNaMesa[ maiorCartaTime1 ] , pMesa->vtCartasNaMesa[ 2 * i ] , pMesa->cartaManilha , &maiorCarta ) == BAR_CondRetCartaNaoExiste )
+        {
+            return MES_CondRetCartaNaoExiste ;
+        } /* if */
+
+        if ( maiorCarta == 2 )
+        {
+            maiorCartaTime1 = 2 * i ;
+        } /* if */
+
+        if ( BAR_IdentificaMaior( pMesa->vtCartasNaMesa[ maiorCartaTime2 ] , pMesa->vtCartasNaMesa[ 2 * i + 1 ] , pMesa->cartaManilha , &maiorCarta ) == BAR_CondRetCartaNaoExiste )
+        {
+            return MES_CondRetCartaNaoExiste ;
+        } /* if */
+
+        if ( maiorCarta == 2 )
+        {
+            maiorCartaTime2 = 2 * i + 1 ;
+        } /* if */
+    }
+
+    if ( BAR_IdentificaMaior ( pMesa->vtCartasNaMesa[ maiorCartaTime1 ] , pMesa->vtCartasNaMesa[ maiorCartaTime2 ] , pMesa->cartaManilha , &maiorCarta ) == BAR_CondRetCartaNaoExiste )
+    {
+        return MES_CondRetCartaNaoExiste ;
+    } /* if */
+
+    * timeGanhador = maiorCarta ;
+
+    return MES_CondRetOk ;
+    /* Fim função: MES Identificar Ganhador */
 }
